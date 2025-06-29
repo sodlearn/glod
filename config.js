89f16aa5045e24ae536c40f840dfaf4a3af01e7c
sodlearn <110545299+sodlearn@users.noreply.github.com>
2025-06-29 20:00:43 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js - ไฟล์การตั้งค่า
 const LIFF_ID = '2007617196-RZDNBQPe';
-const API_URL = 'https://script.google.com/macros/s/AKfycbwS4EwiW66bx8AKD7bt40vcFbZK7xpCod46EeomxBGGfFF_2YARo0xASMSmbPBVnKY0/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbw9hy4NDMllUCZiSaQKFgMGaMPyFngTHb2wxxeqqQAWL-N-ZHrcRkhPKjO6qL5Lum9h9g/exec';
 const GOLD_PRICE_API = 'https://script.google.com/macros/s/AKfycbwgvstkxFOR9p6zOV2d8iEGagbpQ6h8C3BhPnDCoB56jvmbAwSG0A9a36r6oRxNkBXQ/exec';
 const GRAMS_PER_BAHT = 15.244;
 // const SLIP_FOLDER_ID = '1jOsUGHmNIJ6KxMaLm1O8ENoEnUc9Kzd_';
